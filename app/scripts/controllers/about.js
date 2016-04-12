f35a815cd360e5496ca538fce48f5b8075f7cc84
camus122 <mu122@msn.com>
2016-04-12 00:00:53 -0300

    Se sube la priemra versiond ela estructura generada por yeomen, para una app cliente angularjs

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name marvelousApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the marvelousApp
+ */
+angular.module('marvelousApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name marvelousApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the marvelousApp
+ */
+angular.module('marvelousApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
